262788f75cc492721f22bb263db957d552dacd08
marwijn <marwijn.christiaans@gmail.com>
2017-04-17 15:10:58 +0200

    Basic Communiction with Webapi

--- AureliaTest/src/app.ts
+++ AureliaTest/src/app.ts
@@ -6,7 +6,8 @@ export class App {
   public configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'bootstrap-demo'], name: 'bootstrap-demo', moduleId: 'bootstrap-demo', nav: true, title: 'Bootstrap' }
+      { route: ['', 'bootstrap-demo'], name: 'bootstrap-demo', moduleId: 'bootstrap-demo', nav: true, title: 'Bootstrap' },
+      { route: ['fetch-client-demo'], name: 'fetch-client-demo', moduleId: 'fetch-client-demo', nav: true, title: 'FetchClientDemo' }
     ]);
 
     this.router = router;
---
+++ AureliaTest/src/fetch-client-demo.ts
@@ -0,0 +1,18 @@
+﻿import { autoinject } from "aurelia-framework"
+import { HttpClient } from "aurelia-fetch-client"
+
+
+@autoinject
+export class FetchClientDemo {
+
+  message : string;
+
+  constructor(private http: HttpClient) {
+  }
+
+  async activate() {
+    let data: Response = await this.http.fetch("api/values");
+    let x = await data.json();
+    this.message = x[0].toString();
+  }
+}
